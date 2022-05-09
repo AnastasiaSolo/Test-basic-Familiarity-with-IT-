67f385155562fbf0c78b8aa1a6169cc015437873
Anastasia <neacrizis@gmail.com>
2022-05-09 11:12:01 +0400

    Control

---
+++ Control/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] word = { "Hello", "how", "are", "you", };
+for (int i = 0; i < word.Length; i++)
+    Console.WriteLine(word[i]);
+Console.WriteLine("____________________");
+
+var result = new string[word.Length];
+var Size = 0;
+foreach (var value in word)
+{
+    if (value.Length <= 3)
+    {
+        result[Size] = value;
+        Size++;
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, Size));
